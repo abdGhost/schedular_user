a463ee41f6ff024652668a079198afa295c75954
Abdul Taher Khan <49634197+abdGhost@users.noreply.github.com>
2024-05-03 17:56:07 +0530

    worked on UI

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import SideNav from "./components/ui/SideNav";
+import "./index.css";
+
+export default function App() {
+  return (
+    <>
+      {/* <CalendarPage /> */}
+      <SideNav />
+    </>
+  );
+}
